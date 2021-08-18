d3bddb40a3e627bd93a7e86de3d9f8d08839a35d
Ben Ferreira <benjamin@ferreira.si>
2021-08-18 17:04:46 +0200

    add messaging demo

--- background.js
+++ background.js
@@ -1,5 +1,25 @@
 //Work in progress - Now being used for testing purposes only
 
+function handleMessage (message, sender, sendResponse) {
+        console.log(`got message: ${JSON.stringify(message, 0, 2)}`)
+        messageResponses[message.message](message, sendResponse)
+
+        return true
+}
+
+const messageResponses = {
+        getTemplates,
+}
+
+function getTemplates (message, cb) {
+        chrome.storage.sync.get(["save"], (res) => {
+                console.log(`retrieved: ${JSON.stringify(res, 0, 2)}`)
+                cb(res.save)
+            })
+}
+      
+chrome.runtime.onMessage.addListener(handleMessage)
+
 chrome.storage.sync.get(null, function callback(items) {
         console.log(items)
 });
